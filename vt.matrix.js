c8e346bfdc10235ab4513e1debb283388b09da95
Roimer Garcia <roimergarcia@gmail.com>
2013-04-07 12:35:07 -0430

    primer commit

---
+++ vt.matrix.js
@@ -0,0 +1,17 @@
+/**
+ * vt.matrix.js
+ *
+ * vt.matrix.js es una librería de funciones para manipulacion básica de matrices.
+ *
+ *
+ * Copyright 2013 Roimer Garcia (@roimergarcia)
+ *
+ * Liberado bajo licencia CC BY-SA 3.0, disponible en:
+ * http://creativecommons.org/licenses/by-sa/3.0/deed.es
+ * 
+ * ------------------------------------------------
+ *  author:  Roimer Garcia
+ *  version: 0.1
+ *  web:     http://variabletecnica.com//
+ *  source:  https://github.com/roimergarcia/vt.matrix.js/
+ */
